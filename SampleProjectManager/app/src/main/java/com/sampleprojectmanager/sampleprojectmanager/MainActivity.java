aa3604cba21db0a99e4ffbba0a1f87205bfe2d25
Marcus Fiuza <marcusvafiuza@gmail.com>
2018-03-23 11:29:46 -0300

    Gradle Adjust / Folder Structure

---
+++ SampleProjectManager/app/src/main/java/com/sampleprojectmanager/sampleprojectmanager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sampleprojectmanager.sampleprojectmanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by marcusfiuza on 23/03/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
